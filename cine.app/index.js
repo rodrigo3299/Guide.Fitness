a5c4a42b23edca4bc7fa0b7f9793183e2c843c7d
rodrigo3299 <rodrigolima7192@gmail.com>
2024-09-30 15:24:02 -0300

    ac

--- cine.app/index.js
+++ cine.app/index.js
@@ -4,25 +4,25 @@ import mongoose from 'mongoose';
 const app = express();
 const PORT = 3000;
 
-app.use(express.json()); // Para analisar JSON no corpo da requisição
+app.use(express.json());
 
-// Conexão com o MongoDB
+
 mongoose.connect('mongodb+srv://rodrigo:rodrigo3277@cluster0.mx80r.mongodb.net/?retryWrites=true&w=majority')
   .then(() => console.log('Conectado ao MongoDB'))
   .catch(err => console.error('Erro de conexão:', err));
 
-// Endpoint de login (POST)
+
 app.post('/login', (req, res) => {
-  // Seu código de autenticação aqui
+  
   res.send('Endpoint de login');
 });
 
-// Endpoint para acessar a página de login (GET)
+
 app.get('/login', (req, res) => {
-  res.send('Página de login'); // Pode ser um HTML ou outro conteúdo
+  res.send('Página de login'); 
 });
 
-// Iniciar o servidor
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
